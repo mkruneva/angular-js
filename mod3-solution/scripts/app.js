01db5783ad6474c855d93549c07f090813fb08d9
mkruneva <marta.kruneva@gmail.com>
2017-07-13 14:54:08 +0100

    method getMatchedMenuItems(searchTerm) added to the service and associated with button click

--- mod3-solution/scripts/app.js
+++ mod3-solution/scripts/app.js
@@ -14,8 +14,12 @@ function NarrowItDownController (MenuSearchService) {
 	var narrowCtrl = this;
 
 	
-	//test if service is properly injected in the controller
-	narrowCtrl.testText = MenuSearchService.getMatchedMenuItems();
+	////test if service is properly injected in the controller
+	//narrowCtrl.testText = MenuSearchService.getMatchedMenuItems();
+
+	narrowCtrl.getItems = function (searchTerm) {
+		MenuSearchService.getMatchedMenuItems(searchTerm);
+	}
 
 
 	// the controller should call the getMatchedMenuItems method when appropriate and store the result in a property called found attached to the controller instance.
@@ -26,10 +30,8 @@ function NarrowItDownController (MenuSearchService) {
 function MenuSearchService () {
 	var service = this;
 
-	var testText = "new text in Service";
-
 	service.getMatchedMenuItems = function (searchTerm) {
-		return testText;
+		console.log("ok, that seems to work and is showing " + searchTerm);
 	}
 
 
